cbdab88a92affe6cd2890d622b92ac96278824a4
Davide Ghilardi <ghilardi.davide@gmail.com>
2019-05-07 02:39:18 +0200

    move spring cloud version to plugin

--- grizzly-parent/grizzly-plugin/src/main/kotlin/org/d71/grizzly/plugin/GrizzlyPlugin.kt
+++ grizzly-parent/grizzly-plugin/src/main/kotlin/org/d71/grizzly/plugin/GrizzlyPlugin.kt
@@ -1,15 +1,21 @@
 package org.d71.grizzly.plugin
 
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.artifacts.DependencyResolutionListener
 import org.gradle.api.artifacts.ResolvableDependencies
+import io.spring.gradle.dependencymanagement.dsl.ImportsHandler
+import com.sun.javafx.scene.CameraHelper.project
+import io.spring.gradle.dependencymanagement.DependencyManagementPlugin
+import org.gradle.api.Action
 
-open class MyExt (
-    val toolVersion: String = "xxx"
+
+open class MyExt(
+        val toolVersion: String = "xxx"
 )
 
-open class GrizzlyPlugin: Plugin<Project> {
+open class GrizzlyPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             val myExt = extensions.create("grizzlyPlugin", MyExt::class.java)
@@ -40,15 +46,31 @@ open class GrizzlyPlugin: Plugin<Project> {
                     // compileDeps.add(project.dependencies.create("org.springframework.cloud:spring-cloud-gcp-starter-trace"))
                     // compileDeps.add(project.dependencies.create("org.springframework.cloud:spring-cloud-gcp-starter-logging"))
                     // compileDeps.add(project.dependencies.create("org.springframework.cloud:spring-cloud-gcp-starter-storage"))
-                    
+
                     println("myConfig=${compileDeps}")
                     gradle.removeListener(this)
                 }
+
                 override fun afterResolve(dependencies: ResolvableDependencies) {}
             })
 
             pluginManager.apply("org.springframework.boot")
             pluginManager.apply("io.spring.dependency-management")
+
+
+            plugins.apply(DependencyManagementPlugin::class.java)
+            extensions
+                    .findByType(DependencyManagementExtension::class.java)!!
+                    .imports(object : Action<ImportsHandler> {
+
+                        override fun execute(importsHandler: ImportsHandler) {
+                            val springCloudVersion = "Greenwich.SR1"
+                            importsHandler.mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+                        }
+
+                    })
+
         }
     }
 }
+
